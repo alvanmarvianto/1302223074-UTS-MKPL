0fa8bdf0c300c829ab7a36377f80aaa7a50ad645
Alvan <alvanmarvianto@gmail.com>
2025-04-19 01:09:49 +0700

    mengubah boolean gender menjadi enum Gender

--- src/main/java/taxcalculator/Employee.java
+++ src/main/java/taxcalculator/Employee.java
@@ -21,7 +21,7 @@ public class Employee {
     private int monthWorkingInYear;
 
     private boolean isForeigner;
-    private boolean gender; //true = Laki-laki, false = Perempuan
+    private Gender gender; //Enum dari Gender.Java
 
     private int monthlySalary;
     private int otherMonthlyIncome;
@@ -33,7 +33,7 @@ public class Employee {
     private List<String> childNames;
     private List<String> childIdNumbers;
 
-    public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, LocalDate joinDate, boolean isForeigner, boolean gender) {
+    public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, LocalDate joinDate, boolean isForeigner, Gender gender) {
         this.employeeId = employeeId;
         this.firstName = firstName;
         this.lastName = lastName;
